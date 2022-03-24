fb70662cac1f0feab618bde720544b63de7a8b69
fodermat <89015383+fodermat@users.noreply.github.com>
2022-03-24 12:23:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aci" {
+  username = "${var.apic_username}"
+  password = "${var.apic_password}"
+  url      = "${var.apic_url}"
+  insecure = true
+}
+
 resource "aci_tenant" "TF_tenant" {
   name = "${var.tenant}"
   description =   "Created by Terraform Cloud"
